c134db7b16cd4811355468a668bd57147e668bf0
Gorkem Ercan <gorkem.ercan@gmail.com>
2022-09-07 17:01:37 -0400

    Initial changes
    Quick and dirty implementation

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,34 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+import * as minimatch from 'minimatch'
+import parse from 'parse-diff'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const token = core.getInput('token')
+    const octokit = github.getOctokit(token)
+    const requiredFilePatterns = core.getMultilineInput('filePatterns')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    if (requiredFilePatterns) {
+      const diff_url = github.context.payload.pull_request?.diff_url
+      const result = await octokit.request(diff_url)
+      const files = parse(result.data)
+      const fileTolist = files.map(file => `${file.to}`)
+      const unmatched = requiredFilePatterns;
+      for (const pattern of requiredFilePatterns) {
+        if (minimatch.match(fileTolist, pattern)) {
+          const patternIndex = unmatched.indexOf(pattern)
+          if (patternIndex > -1) {
+            unmatched.splice(patternIndex, 1)
+          }
+        }
+      }
+      if (unmatched.length > 0) {
+        core.setFailed(
+          `All required file patterns are not satisfied. No matching files for pattern ${unmatched[0]}`
+        )
+      }
+    }
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
